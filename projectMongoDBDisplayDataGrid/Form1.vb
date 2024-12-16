8f0a9730173a0e4d5dd001b06c331768f1397790
jasaga2401 <jasaga@protonmail.com>
2024-12-16 15:48:58 +0000

    Add project files.

---
+++ projectMongoDBDisplayDataGrid/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports MongoDB.Bson
+Imports MongoDB.Driver
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim connectionString As String = "mongodb+srv://jasaga:igywFraEIYcgiMZG@cluster0.sd0fg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+        ' Create a MongoClient
+        Dim client As New MongoClient(connectionString)
+
+        ' Access the database
+        Dim database As IMongoDatabase = client.GetDatabase("dbcustomer")
+
+        ' Access the collection
+        Dim collection As IMongoCollection(Of BsonDocument) = database.GetCollection(Of BsonDocument)("users")
+
+        ' Define a filter to search for "forename" = "Kenny"
+        Dim filter As FilterDefinition(Of BsonDocument) = Builders(Of BsonDocument).Filter.Eq(Of String)("forename", "Kenny")
+
+        ' Execute the query
+        Dim documents As List(Of BsonDocument) = collection.Find(filter).ToList()
+
+        ' Convert to a DataTable for display
+        Dim dataTable As New DataTable()
+        dataTable.Columns.Add("ID")
+        dataTable.Columns.Add("Forename")
+        dataTable.Columns.Add("Surname")
+
+        For Each document In documents
+            Dim id = document("_id").ToString()
+            Dim forename = document("forename").AsString
+            Dim surname = document("surname").AsString
+            dataTable.Rows.Add(id, forename, surname)
+        Next
+
+        ' Bind the data to a DataGridView
+        DataGridView1.DataSource = dataTable
+
+
+
+
+    End Sub
+End Class
---
+++ projectMongoDBDisplayDataGrid/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("projectMongoDBDisplayDataGrid")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("projectMongoDBDisplayDataGrid")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("69bb7a4c-4411-48a9-bb73-4c1f0bd1d33e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
